723d3c33679a16e872c45253c0a46410a18c741d
Roman-Fedotov-iOS <romafed23@gmail.com>
2022-05-26 10:52:23 +0300

    Initial Commit

---
+++ Crypto-Test/Crypto_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TestApp.swift
+//  Crypto-Test
+//
+//  Created by Roman Fedotov on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
